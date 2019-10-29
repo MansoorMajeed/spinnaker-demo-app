320fe8124f2a6e7afb5464f26a6d1f00cd1d04cc
Mansoor A <mansoor.a@livestream.com>
2019-10-29 18:02:19 +0530

    k8s, v3

--- main.go
+++ main.go
@@ -11,5 +11,5 @@ func main() {
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, this is app version: 2")
+    fmt.Fprintf(w, "Hello, this is app version: 3")
 }
